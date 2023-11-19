6cf2c9e4eae85ced09de41eff38deacc5d1abcfb
mohasinakonda <mohasinakonda39@gmail.com>
2023-11-19 21:09:28 +0600

    chore: add factory seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Book;
+use App\Models\Review;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +14,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Book::factory(33)->create()->each(function ($book) {
+            $bookCount = random_int(5, 30);
+            Review::factory()->count($bookCount)->good()->for($book)->create();
+        });
+        Book::factory(34)->create()->each(function ($book) {
+            $bookCount = random_int(5, 30);
+            Review::factory()->count($bookCount)->average()->for($book)->create();
+        });
+        Book::factory(33)->create()->each(function ($book) {
+            $bookCount = random_int(5, 30);
+            Review::factory()->count($bookCount)->bad()->for($book)->create();
+        });
     }
 }
