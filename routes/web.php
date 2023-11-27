5a1bf09fbc79db09571d68b90482ce8bb20ad0df
mohasinakonda <mohasinakonda39@gmail.com>
2023-11-27 21:41:46 +0600

    chore: redirect home page to books page

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('books');
+    return redirect()->route('books.index');
 });
 
 Route::resource('books', BookController::class);
